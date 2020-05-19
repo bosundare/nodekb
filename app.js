61093fb8f31988f8fe70feb59a8eb38fd68fd81c
Bosun Dare <root@PC0YPGR1.corp.nutanix.com>
2020-05-19 00:41:25 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/nodekb', { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+
+let db = mongoose.connection;
+
+db.once('open', function(){
+  console.log('connected to MondoDB')
+});
+
+db.on('error', function(err){
+  console.log(err)
+});
+
+let Article = require('./models/article');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Body Parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+
+app.get('/', function(req, res){
+  Article.find({}, function(err, articles){
+    if(err){
+      console.log(err);
+    } else {
+      res.render('index', {
+        title:'Articles',
+        articles: articles
+      });
+    }
+  });
+});
+
+let articles = require('./routes/articles');
+app.use('/articles', articles)
+
+app.listen(3000, function(){console.log('Servier started on localhost:3000')});
---
+++ routes/articles.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const router = express.Router();
+
+
+let Article = require('../models/article');
+
+router.get('/add', function (req,res) {
+  res.render('add_article', {
+    title: 'Add Article'
+  });
+});
+
+router.get('/edit/:id', function(req, res){
+  Article.findById(req.params.id, function(err, article){
+    res.render('edit_article', {
+      title: 'Edit article',
+      article:article
+  });
+});
+});
+
+// app.post('/articles/add', function(req,res){
+//   console.log(req.body.author);
+//   return;
+// });
+router.post('/add', function(req, res){
+  req.checkBody('title','Title is required').notEmpty();
+  req.checkBody('author','Author is required').notEmpty();
+  req.checkBody('body','Body is required').notEmpty();
+
+  let errors = req.validationErrors();
+
+  if(errors){
+    res.render('add_article', {
+      errors:errors
+    });
+  } else {
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+    article.save(function(err){
+      if(err){
+        console.log(err);
+        return;
+      } else {
+        req.flash('success', 'Ariticle Added')
+        res.redirect('/');
+      }
+    });
+  }
+
+
+});
+
+router.post('/edit/:id', function(req, res){
+  let article = {}
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+  let query ={_id:req.params.id}
+
+  Article.updateOne(query, article, function(err){
+    if(err){
+      req.flash('danger', 'Unable to Update Article')
+      console.log(err);
+      return;
+    } else {
+      req.flash('success', 'Article Updated')
+      res.redirect('/');
+    }
+  });
+});
+
+router.delete('/:id', function(req, res){
+  let query = {_id:req.params.id}
+
+  Article.deleteOne(query, function(err){
+    if(err){
+      req.flash('danger', 'Unable to Delete Article')
+      console.log(err);
+    }
+    res.send('Success');
+  });
+});
+
+router.get('/:id', function(req, res){
+  Article.findById(req.params.id, function(err, article){
+    res.render('article', {
+      article:article
+  });
+});
+});
+
+
+
+module.exports = router;
